c09a8e7160566589084a509bc018c83ab0b2382b
otherJL0 <jonathanglopez@gmail.com>
2021-09-30 21:11:50 -0400

    Fixed clang formatting

--- 01_greeting/greeting.c
+++ 01_greeting/greeting.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2)
-    puts("Hello there");
-  else
-    printf("Hello %s\n", argv[1]);
+    if (argc < 2)
+        puts("Hello there");
+    else
+        printf("Hello %s\n", argv[1]);
 
-  return (0);
+    return (0);
 }
