65945d4313978881419567e6ef280dee9eb0714f
Jonathan Lopez <jonathanglopez@gmail.com>
2022-06-03 15:06:57 -0400

    Adding option to pass username

--- 01_greeting/greetings.c
+++ 01_greeting/greetings.c
@@ -3,8 +3,8 @@
 
 #define PROJECT_NAME "greetings"
 
-int main() {
-  const char *user = getenv("USER");
+int main(int argc, char *argv[]) {
+  const char *user = (argc < 2) ? getenv("USER") : argv[1];
   printf("Hello, %s!\n", user);
   return EXIT_SUCCESS;
 }
