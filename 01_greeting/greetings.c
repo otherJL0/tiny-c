e7cfddb550bea3c799aa7f51dfe197b038f93e59
Jonathan Lopez <jonathanglopez@gmail.com>
2022-06-03 15:01:46 -0400

    Use environment variable USER for greeting

--- 01_greeting/greetings.c
+++ 01_greeting/greetings.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PROJECT_NAME "greetings"
 
-int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
-    printf("This is project %s.\n", PROJECT_NAME);
-    return 0;
+int main() {
+  const char *user = getenv("USER");
+  printf("Hello, %s!\n", user);
+  return EXIT_SUCCESS;
 }
