ea2ba7e62231561ce0536da0121e325d38947692
Jonathan Lopez <jonathanglopez@gmail.com>
2022-06-08 08:45:03 -0400

    Adding option to pass in phrase through command line args

--- nato/nato.c
+++ nato/nato.c
@@ -1,21 +1,27 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PROJECT_NAME "nato"
 
-int main() {
+const char *nato[] = {
+    "Alpha",  "Bravo",   "Charlie",  "Delta",  "Echo",   "Foxtrot", "Golf",
+    "Hotel",  "India",   "Juliette", "Kilo",   "Lima",   "Mike",    "November",
+    "Oscar",  "Papa",    "Quebec",   "Romeo",  "Sierra", "Tango",   "Uniform",
+    "Victor", "Whiskey", "Xray",     "Yankee", "Zulu"};
 
-  const char *nato[] = {"Alpha",   "Bravo",  "Charlie", "Delta",    "Echo",
-                        "Foxtrot", "Golf",   "Hotel",   "India",    "Juliette",
-                        "Kilo",    "Lima",   "Mike",    "November", "Oscar",
-                        "Papa",    "Quebec", "Romeo",   "Sierra",   "Tango",
-                        "Uniform", "Victor", "Whiskey", "Xray",     "Yankee",
-                        "Zulu"};
+int main(int argc, char *argv[]) {
 
   char phrase[64];
-  printf("Enter a word or phrase: ");
-  fgets(phrase, 64, stdin);
+  if (argc < 2) {
+    printf("Enter a word or phrase: ");
+    fgets(phrase, 64, stdin);
+  } else {
+    for (int i = 1; i < argc; i++) {
+      strcat(phrase, argv[i]);
+    }
+  }
 
   char ch;
   for (int i = 0; i < 64; i++) {
